a323d3eb6011ed494337a91bc1967892b2c62af0
michikowidjaya <michiko.widjaya3@gmail.com>
2025-06-23 15:04:06 +0700

    Delete proyek-kopi-backend directory

--- proyek-kopi-backend/routes/admin.routes.js
+++
@@ -1,9 +0,0 @@
-// File: routes/admin.routes.js
-const express = require('express');
-const router = express.Router();
-const adminController = require('../controllers/admin.controller');
-
-// Rute untuk admin menambah produk baru
-router.post('/products', adminController.tambahProdukBaru);
-
-module.exports = router;
